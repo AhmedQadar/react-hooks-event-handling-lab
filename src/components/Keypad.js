e20ce385b69fa6d594a4eebb67f471ec70ab56aa
ahmedqadar <abdulnet029@gmail.com>
2023-11-08 21:09:42 +0300

    event-handling

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    const handleFocus = () => {
+        console.log('Entering password...');
+    }
     return (
-        <div></div>
+        <div>
+        <input
+        placeholder="Password"
+        type="password"
+        onChange={handleFocus}
+        ></input>
+        </div>
     )
 }
 
